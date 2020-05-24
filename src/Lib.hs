55d084a1c743c45f8e08618d9f957b4bd03e6470
waltont8 <2472981+waltont8@users.noreply.github.com>
2020-05-24 14:15:57 +0100

    Cmd line + some extra readout

--- src/Lib.hs
+++ src/Lib.hs
@@ -6,6 +6,7 @@ import Data.Time          -- For the UTCTime type
 import Data.Time.ISO8601  -- Parse the datetime format in gpx files
 import Data.Time.Clock
 import System.Time.Utils (renderSecs)
+import System.Environment
 
 import Gpx
 
@@ -15,7 +16,11 @@ showResult :: Result -> String
 showResult (d,t) = "(" ++ (show d) ++ "Km, " ++(renderS t) ++ ")"
 
 someFunc = do
-            route <- getRoute "activity.gpx"
-            putStrLn $ show $ totalDistance route
-            putStrLn $ renderS $ totalTime route
-            putStrLn $ showResult $ fastestNk 5 (trackPoints route)
+            args <- getArgs
+            route <- getRoute $ if length args > 0 then head args else "activity.gpx"
+            putStrLn $ "Total distance " ++ (show $ totalDistance route)
+            putStrLn $ "Total time " ++ (renderS $ totalTime route)
+            putStrLn $ "Best 1k " ++ (showResult $ fastestNk 1 (trackPoints route))
+            putStrLn $ "Best 5k " ++ (showResult $ fastestNk 5 (trackPoints route))
+            putStrLn $ "Best 10k " ++ (showResult $ fastestNk 10 (trackPoints route))
+            putStrLn $ "Best mile " ++ (showResult $ fastestNk 1.609344 (trackPoints route))
